9351044c957be40593b4330c6abfc008a7ddbe46
boapi <tobias.weyer1205@gmail.com>
2025-01-14 17:50:39 +0100

    * add internal lib to handle poekapi * add baseURL * add calling of api * add types for JSON decoding

---
+++ internal/pokeapi/location_list.go
@@ -0,0 +1,38 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
+	url := baseURL + "/location-area"
+	if pageURL != nil {
+		url = *pageURL
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+	defer response.Body.Close()
+
+	dat, err := io.ReadAll(response.Body)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	locationsResp := RespShallowLocations{}
+	err = json.Unmarshal(dat, &locationsResp)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	return locationsResp, nil
+}
